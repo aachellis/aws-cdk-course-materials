107bb9a933b340b655a23177a652a13e107b2c8f
aachellis <abhijit1991@hotmail.com>
2022-01-04 16:25:40 +0530

    Made aspect class

--- aspect_demo/aspect_demo/aspect_demo_stack.py
+++ aspect_demo/aspect_demo/aspect_demo_stack.py
@@ -1,9 +1,15 @@
 from aws_cdk import (
     # Duration,
     Stack,
+    aws_s3 as s3,
+    aws_iam as iam,
+    aws_lambda as lambda_,
+    Aspect,
+    aws_s3_notifications,
     # aws_sqs as sqs,
 )
 from constructs import Construct
+import jsii
 
 class AspectDemoStack(Stack):
 
@@ -11,9 +17,49 @@ class AspectDemoStack(Stack):
         super().__init__(scope, construct_id, **kwargs)
 
         # The code that defines your stack goes here
+        role = iam.Role(
+            self, 'lambda_role',
+            assumed_by=iam.ServicePrincipal("lambda.amazonaws.com"),
+            role_name="lambda_role"
+        )
 
-        # example resource
-        # queue = sqs.Queue(
-        #     self, "AspectDemoQueue",
-        #     visibility_timeout=Duration.seconds(300),
-        # )
+        role.add_managed_policy(
+            iam.ManagedPolicy.from_aws_managed_policy_name("AmazonS3FullAccess")
+        )
+        role.add_managed_policy(
+            iam.ManagedPolicy.from_aws_managed_policy_name("service-role/AWSLambdaBasicExecutionRole")
+        )
+
+        # Create s3 bucket
+        source_bucket = s3.Bucket(
+            self, "cdk-source-bucket",
+            bucket_name="demo-source-865"
+        )
+        dest_bucket = s3.Bucket(
+            self, "cdk-dest-bucket",
+            bucket_name="demo-dest-865"
+        )
+
+        # Create Lambda function
+        function = lambda_.Function(
+            self, "lambda_function",
+            runtime=lambda_.Runtime.PYTHON_3_7,
+            handler="lambda-handler.main",
+            environment={
+                'DestinationBucket': dest_bucket.bucket_name
+            },
+            role=role, 
+            code=lambda_.Code.from_asset("./lambda")
+        )
+
+        # Create s3 notification for lambda function
+        notification = aws_s3_notifications.LambdaDestination(function)
+
+        # Assign notification for the s3 event type (ex: OBJECT_CREATED)
+        source_bucket.add_object_created_notification(notification)        
+
+@jsii.implements(IAspect)
+class EncryptionAspect:
+    def visit(self, construct):
+        print("------")
+        print(type(construct))
