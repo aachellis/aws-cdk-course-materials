cb6e86443efc10c0c6d3b1d2f157fa1ae6b3cd2a
aachellis <abhijit1991@hotmail.com>
2022-01-04 15:41:22 +0530

    using multiple environments

--- demo_app/app.py
+++ demo_app/app.py
@@ -5,24 +5,17 @@ import aws_cdk as cdk
 
 from demo_app.demo_app_stack import DemoAppStack
 
+default = cdk.Environment(account='386768119616', region='ap-south-1')
+prod = cdk.Environment(account='725124995909', region='ap-south-1')
+
 
 app = cdk.App()
 DemoAppStack(app, "DemoAppStack",
-    # If you don't specify 'env', this stack will be environment-agnostic.
-    # Account/Region-dependent features and context lookups will not work,
-    # but a single synthesized template can be deployed anywhere.
+    env=dafault, prod_env=False
+)
 
-    # Uncomment the next line to specialize this stack for the AWS Account
-    # and Region that are implied by the current CLI configuration.
-
-    #env=cdk.Environment(account=os.getenv('CDK_DEFAULT_ACCOUNT'), region=os.getenv('CDK_DEFAULT_REGION')),
-
-    # Uncomment the next line if you know exactly what Account and Region you
-    # want to deploy the stack to. */
-
-    env=cdk.Environment(account='386768119616', region='ap-south-1'),
-
-    # For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html
-    )
+DemoAppStack(app, "DemoAppStack",
+    env=prod, prod_env=True
+)
 
 app.synth()
--- demo_app/demo_app/demo_app_stack.py
+++ demo_app/demo_app/demo_app_stack.py
@@ -2,17 +2,67 @@ from aws_cdk import (
     # Duration,
     Stack,
     aws_s3 as s3,
+    aws_iam as iam,
+    aws_lambda as lambda_,
+    aws_s3_notification,
     # aws_sqs as sqs,
 )
 from constructs import Construct
 
 class DemoAppStack(Stack):
 
-    def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
+    def __init__(self, scope: Construct, construct_id: str, prod_env=False, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
         # The code that defines your stack goes here
-        s3.Bucket(
-            self, 'cdk-source-bucket'
+        # Lambda Role
+        role = iam.Role(
+            self, 'lambda_role',
+            assumed_by=iam.ServicePrincipal("lambda.amazonaws.com"),
+            role_name="lambda_role"
         )
-        
+
+        role.add_managed_policy(
+            iam.ManagedPolicy.from_aws_managed_policy_name("AmazonS3FullAccess")
+        )
+        role.add_managed_policy(
+            iam.ManagedPolicy.from_aws_managed_policy_name("AWSLambdaBasicExecutionRole")
+        )
+
+        # Create s3 bucket
+        if prod_env:
+            source_bucket = s3.Bucket(
+                self, "cdk-source-bucket",
+                bucket_name="demo-source-865-prod"
+            )
+            dest_bucket = s3.Bucket(
+                self, "cdk-dest-bucket",
+                bucket_name="demo-dest-865-prod"
+            )
+        else:
+            source_bucket = s3.Bucket(
+                self, "cdk-source-bucket",
+                bucket_name="demo-source-865-nonprod"
+            )
+            dest_bucket = s3.Bucket(
+                self, "cdk-dest-bucket",
+                bucket_name="demo-dest-865-nonprod"
+            )
+
+        # Create Lambda function
+        function = lambda_.Function(
+            self, "lambda_function",
+            runtime=lambda_.Runtime.PYTHON_3_7,
+            handler="lambda-handler.main",
+            environment={
+                'DestinationBucket': dest_bucket.bucket_name
+            },
+            role=role, 
+            code=lambda_.Code.asset("./lambda")
+        )
+
+        # Create s3 notification for lambda function
+        notification = aws_s3_notification.LambdaDestination(function)
+
+        # Assign notification for the s3 event type (ex: OBJECT_CREATED)
+        source_bucket.add_object_created_notification(notification)
