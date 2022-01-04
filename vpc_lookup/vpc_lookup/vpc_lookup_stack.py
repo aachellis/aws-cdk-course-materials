9a01f3f2af1601c7328fa6e6e5cd369583c4ab60
aachellis <abhijit1991@hotmail.com>
2022-01-04 12:29:12 +0530

    Getting only default vpc by is_default switch

--- vpc_lookup/vpc_lookup/vpc_lookup_stack.py
+++ vpc_lookup/vpc_lookup/vpc_lookup_stack.py
@@ -13,8 +13,8 @@ class VpcLookupStack(Stack):
         super().__init__(scope, construct_id, **kwargs)
 
         # The code that defines your stack goes here
-        vpcid = self.node.try_get_context("vpcid")
-        vpc = ec2.Vpc.from_lookup(self, "VPC", vpc_id=vpcid)
+        # vpcid = self.node.try_get_context("vpcid")
+        vpc = ec2.Vpc.from_lookup(self, "VPC", is_default=True)
 
         subnets = vpc.select_subnets(subnet_type=ec2.SubnetType.PUBLIC)
 
